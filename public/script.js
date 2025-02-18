3f3c79c1aef8b5838fa65b77bdb7b2105f646079
Allugamer123 <aleksikeranen2012@gmail.com>
2025-02-18 19:53:54 +0200

    Neljäs commit

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-document.getElementById("new-game-btn").addEventListener('click,startGame');
+document.getElementById("new-game-btn").addEventListener('click',startGame);
 function startGame(){
    console.log("klikattu")
 }
